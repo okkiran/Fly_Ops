372dccebfff5fd5ee3b8abc90acafd5758551733
okilickiran <ozlemkkiran@gmail.com>
2024-09-30 01:36:03 +0300

    Initial commit for frontend - three js initializations with Reach

---
+++ flightmanagement/frontend3d/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Airplane } from "./components/Airplane";
+import { OrbitControls } from "@react-three/drei"; // Optional, for controlling the camera
+import { Canvas } from "@react-three/fiber";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <Canvas camera={{ position: [0, 5, 10], fov: 50 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
+        <OrbitControls /> {/* Optional, for camera movement */}
+        <Airplane position={[0, 0, 0]} />
+      </Canvas>
+
+  )
+}
+
+export default App
---
+++ flightmanagement/frontend3d/src/components/Airplane.jsx
@@ -0,0 +1,14 @@
+import {useGLTF} from "@react-three/drei";
+import React from "react";
+
+export function Airplane(props) {
+    const {nodes, materials} = useGLTF("./models/Airplane.glb");
+
+    return (
+        <group {...props} dispose={null} scale={[0.02, 0.02, 0.02]}> {/* Adjust scale here */}
+            <mesh geometry={nodes.Boeing_787_8obj2.children[0].geometry} material={materials["Mat"]}/>
+            <mesh geometry={nodes.Boeing_787_8obj2.children[1].geometry} material={materials["Mat.1"]}/>
+        </group>
+    );
+}
+useGLTF.preload("./models/Airplane.glb");
