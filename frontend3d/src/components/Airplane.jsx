9c9c16a7a268b07286d85d00fd68e18bcc058483
okilickiran <ozlemkkiran@gmail.com>
2024-10-14 20:50:35 +0300

    onPositionChange to update the menu position

--- frontend3d/src/components/Airplane.jsx
+++ frontend3d/src/components/Airplane.jsx
@@ -4,14 +4,14 @@ import React, { useRef } from "react";
 import { useGLTF, Float } from "@react-three/drei";
 import * as THREE from "three";
 
-export function Airplane({ scrollProgress, ...props }) {
+export function Airplane({ scrollProgress, onPositionChange, ...props }) {
     const { nodes, materials } = useGLTF("./models/Airplane.glb");
     const airplaneRef = useRef();
     const CURVE_DISTANCE = 100;
 
     // Define the path using CatmullRomCurve3
     const path = new CatmullRomCurve3([
-        new THREE.Vector3(-80, 0, -100),
+        new THREE.Vector3(0, 0, -CURVE_DISTANCE),
         new THREE.Vector3(0, 0, -CURVE_DISTANCE),
         new THREE.Vector3(150, 0, -2 * CURVE_DISTANCE),
         new THREE.Vector3(-150, 0, -3 * CURVE_DISTANCE),
@@ -28,20 +28,23 @@ export function Airplane({ scrollProgress, ...props }) {
         // Move airplane to the point on the path
         airplaneRef.current.position.copy(pointOnPath);
 
+        // Call onPositionChange to update the menu position
+        if (onPositionChange) {
+            onPositionChange(pointOnPath);
+        }
+
         const tangent = path.getTangentAt(scrollProgress).normalize(); // Calculate direction
         airplaneRef.current.lookAt(pointOnPath.clone().add(tangent)); // Make airplane look ahead
     });
 
     return (
-
-    <group ref={airplaneRef} {...props} scale={[0.2, 0.2, 0.2]} position={[0, 0.1, 0]}>
-        <Float floatIntensity={1.5} speed={1.5} rotationIntensity={0.5}>
-            <mesh geometry={nodes.Boeing_787_8obj2.children[0].geometry} material={materials["Mat"]}/>
-            <mesh geometry={nodes.Boeing_787_8obj2.children[1].geometry} material={materials["Mat.1"]}/>
-        </Float>
-    </group>
-
-);
+        <group ref={airplaneRef} {...props} scale={[0.1, 0.1, 0.1]} position={[0, 0, 0]}>
+            <Float floatIntensity={1.5} speed={1.5} rotationIntensity={1}>
+                <mesh geometry={nodes.Boeing_787_8obj2.children[0].geometry} material={materials["Mat"]}/>
+                <mesh geometry={nodes.Boeing_787_8obj2.children[1].geometry} material={materials["Mat.1"]}/>
+            </Float>
+        </group>
+    );
 }
 
 useGLTF.preload("./models/Airplane.glb");
