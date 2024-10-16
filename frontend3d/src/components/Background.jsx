3cf936a42e3b2558b4450ae35bd7b5a39253b3a8
okilickiran <ozlemkkiran@gmail.com>
2024-10-16 19:57:41 +0300

    Update cloud position

--- frontend3d/src/components/Background.jsx
+++ frontend3d/src/components/Background.jsx
@@ -1,7 +1,7 @@
 // Background.jsx
 import React from 'react';
 import { Sky } from "@react-three/drei";
-import Cloud from './Cloud'; // Import the Cloud component
+import Cloud from './Cloud'; // Import the new Cloud component
 
 const Background = () => {
     return (
@@ -9,12 +9,9 @@ const Background = () => {
             {/* Sky */}
             <Sky distance={450000} sunPosition={[0, 1, 0]} azimuth={0.25} inclination={0.6} />
 
-            {/* Manually positioned clouds around the curve */}
-            <Cloud position={[-30, 10, -150]} scale={1.5} />
-            <Cloud position={[30, 12, -200]} scale={1.8} />
-            <Cloud position={[-10, 11, -250]} scale={1.6} />
-            <Cloud position={[40, 9, -300]} scale={2.0} />
-            <Cloud position={[0, 13, -350]} scale={1.7} />
+            {/* Clouds positioned around the curve */}
+            <Cloud position={[-30, 10, -50]} scale={2} />
+
         </>
     );
 };
