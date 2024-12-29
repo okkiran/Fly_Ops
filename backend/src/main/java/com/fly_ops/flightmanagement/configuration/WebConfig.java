fe92a049a4b1f264382057499c00e7df3d5e1734
okilickiran <ozlemkkiran@gmail.com>
2024-12-29 15:46:41 +0300

    Add WebConfig for CORS

---
+++ backend/src/main/java/com/fly_ops/flightmanagement/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.fly_ops.flightmanagement.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:5173") // Allow requests from the frontend's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (if needed)
+    }
+}
